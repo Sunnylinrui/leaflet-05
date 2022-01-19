7333292d5e9e48ac41c4cf3ef2a224832a2d73ab
Sunnylinrui <2905437435@qq.com>
2022-01-19 14:38:38 +0800

    3 files modified

--- index.js
+++ index.js
@@ -53,33 +53,34 @@ items.forEach((item) => {
 const svg =
   '<svg t="1642557630522" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1435" width="200" height="200"><path d="M487.424 940.032c-120.832 0-323.584-20.48-323.584-102.4s202.752-104.448 323.584-104.448 323.584 22.528 323.584 104.448-202.752 102.4-323.584 102.4z m0-159.744c-182.272 0-272.384 40.96-276.48 57.344 4.096 16.384 94.208 57.344 276.48 57.344s272.384-40.96 276.48-57.344c-4.096-16.384-94.208-57.344-276.48-57.344z m276.48 57.344z" fill="#EE9B9B" p-id="1436"></path><path d="M489.472 79.872c-145.408 0-262.144 116.736-262.144 262.144s262.144 512 262.144 512 262.144-366.592 262.144-512S634.88 79.872 489.472 79.872z m0 272.384c-32.768 0-57.344-24.576-57.344-57.344 0-32.768 24.576-57.344 57.344-57.344 32.768 0 57.344 24.576 57.344 57.344 0 32.768-26.624 57.344-57.344 57.344z" fill="#FDDFDF" p-id="1437"></path></svg>';
 
-const marker = new Marker([30.29726893943521, 120.06622904484556], {
-  icon: new Icon({
-    //原始直接引用网页图片地址
-    // iconUrl:
-    //   'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-    iconUrl: 'data:image/svg+xml,' + encodeURIComponent(svg),
-    iconSize: [40, 60],
-    iconAnchor: [20, 40],
-    popupAnchor: [30.29726893943521, 120.06622904484556],
-    tooltipAnchor: [30.29726893943521, 120.06622904484556],
-  }),
-  opacity: 0.8, //透明度
-  draggable: true, //图标是否可以拖拽
-  autoPan: true,
-  // title: new Title({
-  //   '数智交院',
-  // }),
-});
+// const marker = new Marker([30.29726893943521, 120.06622904484556], {
+//   icon: new Icon({
+//     //原始直接引用网页图片地址
+//     // iconUrl:
+//     //   'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+//     iconUrl: 'data:image/svg+xml,' + encodeURIComponent(svg),
+//     iconSize: [40, 60],
+//     iconAnchor: [20, 40],
+//     popupAnchor: [30.29726893943521, 120.06622904484556],
+//     tooltipAnchor: [30.29726893943521, 120.06622904484556],
+//   }),
+//   opacity: 0.8, //透明度
+//   draggable: true, //图标是否可以拖拽
+//   autoPan: true,
+//   // title: new Title({
+//   //   '数智交院',
+//   // }),
+// });
 // marker.bindPopup(popupContent).openPopup();
 
-const popup = new Popup({
-  maxWidth: 300,
-  maxHeight: 500,
-});
+// const popup = new Popup({
+//   maxWidth: 300,
+//   maxHeight: 500,
+// });
 
-marker.addTo(map); //这句别忘记
+// marker.addTo(map); //这句别忘记
 
+//杭州数据
 const data = {
   type: 'FeatureCollection',
   features: [
@@ -116,7 +117,44 @@ const data = {
   ],
 };
 
-const glayer = new GeoJSON(data, {
+//宁波数据
+const data2 = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      properties: {
+        NAME: '庄桥机场',
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [121.57333374023438, 29.920720633295886],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        NAME: '栎社机场',
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [121.46347045898438, 29.825752664242177],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        NAME: '文化广场',
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [121.6124939918518, 29.860055014492072],
+      },
+    },
+  ],
+};
+
+const glayer1 = new GeoJSON(data, {
   pointToLayer: (geoJsonPoint, lating) => {
     return new Marker(lating, {
       icon: new Icon({
@@ -129,6 +167,42 @@ const glayer = new GeoJSON(data, {
       }),
     });
   },
-})
+});
 
-glayer.addTo(map)
+glayer1.addTo(map);
+
+//事件监听
+const check1 = document.getElementById('check1');
+check1.onchange = (evt) => {
+  if (evt.target.checked) {
+    glayer1.addTo(map);
+  } else {
+    glayer1.removeFrom(map);
+  }
+};
+
+const glayer2 = new GeoJSON(data2, {
+  pointToLayer: (geoJsonPoint, lating) => {
+    return new Marker(lating, {
+      icon: new Icon({
+        //原始直接引用网页图片地址
+        // iconUrl:
+        //   'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+        iconUrl: 'data:image/svg+xml,' + encodeURIComponent(svg),
+        iconSize: [40, 60],
+        iconAnchor: [20, 40],
+      }),
+    });
+  },
+});
+
+glayer2.addTo(map);
+
+const check2 = document.getElementById('check2');
+check2.onchange = (evt) => {
+  if (evt.target.checked) {
+    glayer2.addTo(map);
+  } else {
+    glayer2.removeFrom(map);
+  }
+};
